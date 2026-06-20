1cc3eff095e91796e6e3a6f36ff9d9681050e391
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-20 19:51:00 +0200

    full 26.2.x port

--- Common/src/main/java/fuzs/easyshulkerboxes/common/data/ModItemStorageDefinitionsProvider.java
+++ Common/src/main/java/fuzs/easyshulkerboxes/common/data/ModItemStorageDefinitionsProvider.java
@@ -17,6 +17,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.ColorCollection;
 
 import java.util.function.BiConsumer;
 
@@ -36,39 +37,10 @@ public abstract class ModItemStorageDefinitionsProvider extends AbstractItemStor
     }
 
     public final void registerShulkerBoxes(HolderLookup.RegistryLookup<Item> itemLookup) {
-        this.add(new ContainerStorage(null, StorageOptions.DEFAULT.setFilterContainerItems()), Items.SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.WHITE),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.WHITE_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.ORANGE),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.ORANGE_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.MAGENTA),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.MAGENTA_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.LIGHT_BLUE),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.LIGHT_BLUE_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.YELLOW),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.YELLOW_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.LIME),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.LIME_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.PINK),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.PINK_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.GRAY),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.GRAY_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.LIGHT_GRAY),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.LIGHT_GRAY_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.CYAN),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.CYAN_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.PURPLE),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.PURPLE_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.BLUE),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.BLUE_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.BROWN),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.BROWN_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.GREEN),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.GREEN_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.RED),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.RED_SHULKER_BOX);
-        this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(DyeColor.BLACK),
-                StorageOptions.DEFAULT.setFilterContainerItems()), Items.BLACK_SHULKER_BOX);
+        this.add(new ContainerStorage(), Items.SHULKER_BOX);
+        ColorCollection.zipApply(ColorCollection.VALUES, Items.DYED_SHULKER_BOX, (DyeColor color, Item item) -> {
+            this.add(new ContainerStorage(DyeBackedColor.fromDyeColor(color), StorageOptions.DEFAULT), item);
+        });
     }
 
     public final void registerEnderChest(HolderLookup.RegistryLookup<Item> itemLookup) {
